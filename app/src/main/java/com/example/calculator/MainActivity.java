c34e13600f92f4b97641d4ac1b62f4d7cd0c3c35
monicaoyugi <monicaoyugi@gmail.com>
2024-08-14 20:48:21 +0300

    cnge dthe calcilation logic

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -16,8 +16,13 @@ import androidx.core.view.WindowInsetsCompat;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText num1EditText, num2EditText;
-    private TextView resultTextView;
+
+    //declare the UI Components
+    EditText num1EditText;
+    EditText num2EditText;
+    Button addbutton;
+
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +35,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        num1EditText = findViewById(R.id.num2EditText);
+
+        //find components by id and assign to main variable
+        num1EditText = findViewById(R.id.num1EditText);
         num2EditText = findViewById(R.id.num2EditText);
         resultTextView = findViewById(R.id.resultTextView);
         Button addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View V) {
-
-                performCalculation();
+                int num1 = Integer.parseInt(num1EditText.getText().toString());
+                int num2 = Integer.parseInt(num2EditText.getText().toString());
+                int sum = num1 + num2;
+                resultTextView.setText("" + sum);
+                //performCalculation();
             }
 
         });
 
     }
-        private void performCalculation() {
-            String num1Str = num1EditText.getText().toString();
-            String num2Str = num2EditText.getText().toString();
+
+    public void onClick(View v) {
+        String num1Str = num1EditText.getText().toString();
+        String num2Str = num2EditText.getText().toString();
 
 
-            if (num1Str.isEmpty() || num2Str.isEmpty()) {
-                Toast.makeText(this, "Please enter both numbers", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
+        if (num1Str.isEmpty() || num2Str.isEmpty()) {
+            Toast.makeText(this, "some fields are empty", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
 
             double num1 = Double.parseDouble(num1Str);
             double num2 = Double.parseDouble(num2Str);
@@ -66,5 +76,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+}
 
 
